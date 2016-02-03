1b7a03b07e7630871683286c4471cbcfd8df7bb6
Sothea Nim <nimsothea@gmail.com>
2016-02-03 14:53:12 -0500

    push latest code changes to NodeJSApplication repo

--- main.js
+++ main.js
@@ -57,36 +57,3 @@ module.exports = {
         });
     }
 };
-
-var scapegoat = require('scapegoat')
-escape = scapegoat.escape,
-        unescape = scapegoat.unescape;
-
-var html = '<h1>Hello World</h1>',
-        escaped = escape(html),
-        unescaped = unescape(escaped);
-
-console.log('html', html, 'escaped', escaped, 'unescaped', unescaped);
-
-/*
-var http = require('http');
-var url = require('url');
-var querystring = require('querystring');
-
-var server = http.createServer(function (req, res) {
-    
-    var params = querystring.parse(url.parse(req.url).query);
-    res.writeHead(200, {"Content-Type": "text/plain"});
-    
-    if ('firstname' in params && 'lastname' in params) {
-        res.write('Your name is ' + params['firstname'] + ' ' + params['lastname']);
-    }
-    else {
-        res.write('You do have a first name and a last name, don\'t you?');
-    }
-    
-    res.end();
-});
-
-server.listen(8080);
-*/
